b36de8cac8d6241173f3fa38b748a1c29152bffb
arkzioprime <www.1592516642@qq.com>
2023-01-16 16:00:26 +0800

    rubens: update modules for RedmiK50

--- device.mk
+++ device.mk
@@ -26,6 +26,7 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_PACKAGES += \
+    FrameworkResOverlay \
     RubensFrameworkResOverlay \
     SystemUIOverlay \
     SettingsOverlay
